3aa15a35ffd331fbedcc5dd84f9870d74cb2422f
aaron <hau.zhongyuan@gmail.com>
2021-04-16 21:04:28 +0800

    env fix

--- project/env/bin/activate.fish
+++ project/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/hauzy.aaron/Desktop/zendesk_prodsec/product_security_challenge/env"
+set -gx VIRTUAL_ENV "/Users/hauzy.aaron/Desktop/zendesk_prodsec/product_security_challenge/project/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
