4c69a7bfda2abea166c292d09949e9420f8c4613
aaron <hau.zhongyuan@gmail.com>
2021-04-17 11:44:59 +0800

    implemented log in and log out

--- project/server.py
+++ project/server.py
@@ -1,10 +1,11 @@
-import os, html, re, bcrypt, logging, jwt
+import os, html, re, bcrypt, logging, jwt , random
 from dotenv import load_dotenv
 from os.path import join, dirname
-from flask import Flask, redirect, url_for, render_template, request, flash, make_response
+from flask import Flask, redirect, url_for, render_template, request, flash, make_response, jsonify, g
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
+from time import sleep
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -22,9 +23,9 @@ csrf.init_app(app)
 db = SQLAlchemy(app)
 
 class Users(db.Model):
-    ''' Models '''
+    """ Model for Users """
     __tablename__ = "users"
-    id = db.Column("id", db.Integer, primary_key=True) 
+    id = db.Column("id", db.Integer, primary_key=True, autoincrement=True) 
     username = db.Column(db.String(30))
     password = db.Column(db.String(100))
     salt =  db.Column(db.String(100))
@@ -32,23 +33,39 @@ class Users(db.Model):
     last_login = db.Column(db.Integer)
     lock_time = db.Column(db.Integer)
 
-    def __init__(self, username, password, salt, login_tries, last_login):
+    def __init__(self, username, password, salt, login_tries, last_login, lock_time):
         self.username = username
         self.password = password
         self.salt = salt
         self.login_tries = login_tries
         self.last_login = last_login
+        self.lock_time = lock_time
 
-def htmlSafe(text) :
-    return html.escape(text)
+class RevokedTokens(db.Model) :
+    """ Model for Revoked JWTs """
+    ___tablename__ = "revoked_jwts"
+    id = db.Column("id", db.Integer, primary_key = True, autoincrement = True)
+    revoked_token = db.Column(db.String(128))
+    revoked_time = db.Column(db.Integer)
+    def __init__(self, revoked_token, revoked_time) :
+        self.revoked_token = revoked_token
+        self.revoked_time = revoked_time
 
 def isUsernameSafe(username) :
+    """
+    Checks if username provided follows the rule : alpha-numeric and '-'
+    Returns : Boolean
+    """
     if re.match(r"^[a-zA-Z0-9 .-]+$", username) :
         return True
     else :
         return False
 
 def isCommonPwd(pwd, file = '10k-most-common.txt') :
+    """
+    Helper function to check for common password from a txt file
+    Returns : Boolean
+    """
     with open('10k-most-common.txt') as f:
         common_pwd = [line.rstrip() for line in f]
     if pwd in common_pwd :
@@ -57,6 +74,11 @@ def isCommonPwd(pwd, file = '10k-most-common.txt') :
         return False
 
 def pwdCheck(pwd):
+    """
+    Helper function to check for validity of a password provided before it is accepted
+    to be use for the account.
+    Returns : Dictionary of key-value boolean pairs of the check result
+    """
     # common password check 
     common_pwd = isCommonPwd(pwd)
 
@@ -88,20 +110,20 @@ def pwdCheck(pwd):
         'symbol_error' : symbol_error,
     }
 
-def encode_auth_token(self, user_id):
+def encode_auth_token(user_id):
     """
     Generates the Auth Token
     :return: string
     """
     try:
         payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=3600),
-            'iat': datetime.datetime.utcnow(),
+            'exp': datetime.utcnow() + timedelta(days=0, seconds=3600),
+            'iat': datetime.utcnow(),
             'sub': user_id
         }
         return jwt.encode(
             payload,
-            app.config.get('SECRET_KEY'),
+            os.environ.get("JWT_SECRET_KEY"),
             algorithm='HS256'
         )
     except Exception as e:
@@ -114,13 +136,17 @@ def decode_auth_token(auth_token):
     :return: integer|string
     """
     try:
-        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+        print("decode",auth_token)
+        payload = jwt.decode(auth_token, os.environ.get("JWT_SECRET_KEY"), algorithm='HS256',
+        options={"verify_signature": False})
+        print(payload)
         return payload['sub']
     except jwt.ExpiredSignatureError:
         return 'Signature expired. Please log in again.'
     except jwt.InvalidTokenError:
         return 'Invalid token. Please log in again.'
 
+""" Routes """
 @app.route("/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
@@ -129,13 +155,63 @@ def login():
         # check safe username
         if isUsernameSafe(username) :
             # perform user check on db
-            user = Users.query.filter_by(username=username).first()
-            current_time = int(datetime.now().timestamp())
-            # TODO: further checks before login
-            return render_template("login.html")
+            user = Users.query.filter_by(username=username).first() 
+            if user :
+                current_time = int(datetime.now().timestamp())
+
+                # check if account is locked out 
+                if user.login_tries > 5  and current_time - user.lock_time < (5*60) :
+                    flash('User Account is Locked Out for ' + str(user.lock_time + (5*60) - current_time) + ' sec', "error")
+                    return redirect(url_for("login"))
+                
+                # check password provided vs stored
+                hash_prov_passwd = bcrypt.hashpw(password.encode(), user.salt)
+                if not hash_prov_passwd == user.password :
+                    # wrong password provided
+                    user.login_tries += 1
+                    if user.login_tries == 5 :
+                        flash('Account Locked For 5min', "error")
+                        app.logger.warning("Account for : " + user.username)
+                        user.lock_time = int(datetime.now().timestamp())
+                        db.session.commit()
+                        sleep(random.uniform(0, 1.5))
+                        return redirect(url_for("login"))
+                    else:   
+                        flash('Please try again', "error")
+                        app.logger.warning("Wrong password provided for : " + user.username)
+                        db.session.commit()
+                        sleep(random.uniform(0, 1.5))
+                        return redirect(url_for("login"))
+                else : 
+                    # correct password provided
+                    sleep(random.uniform(0, 1.5))
+                    try:
+                        auth_token = encode_auth_token(user.id)
+                        print(auth_token)
+                        if auth_token:
+                            user.login_tries = 0
+                            user.last_login = int(datetime.now().timestamp())
+                            db.session.commit()
+                            response = make_response(redirect(url_for("dashboard")))
+                            response.set_cookie('auth', auth_token, httponly=True, secure=True)
+                            flash("Successfully Logged In", "success")
+                            return response
+                    except Exception as e:
+                            print(e)
+                            response = make_response(redirect(url_for("login")))
+                            return response, 500
+            else :
+                # no such user
+                flash("Please try again", "error")
+                app.logger.warning("User does not exist : " + username)
+                sleep(random.uniform(4, 5.5)) # padded time to take into account hashing time for bcrypt
+                return redirect(url_for("login"))
         else:
-            flash("Bad Username: Please try again")
-            return render_template("login.html")
+            # unsafe username
+            flash("Please try again", "error")
+            app.logger.warning("Unsafe username provided : " + username)
+            sleep(random.uniform(4, 5.5)) # padded time to take into account hashing time for bcrypt
+            return redirect(url_for("login"))
     else:
         return render_template("login.html")
 
@@ -146,19 +222,19 @@ def signUp():
             password1 = request.form.get("password1")
             password2 = request.form.get("password2")
             # check safe username
-            if not isUsernameSafe(username) :
-                flash("Bad Username: Please try again")
-                return render_template("create_acc.html") 
+            if not isUsernameSafe(username) or len(username) > 30 :
+                flash("Username should contain only Alpha-numeric characters and hypens and length no more than 30 characters")
+                return redirect(url_for("signUp"))
             # check username does not exist
             user = Users.query.filter_by(username=username).first()
             if user :
-                flash("Bad Username: Please try again")
-                return render_template("create_acc.html")
+                flash("Bad Username: Please try again", "error")
+                return redirect(url_for("signUp"))
 
             # passwords should match
             if password1 != password2 :
                 flash("Password Input Do Not Match")
-                return render_template("create_acc.html")
+                return redirect(url_for("signUp"))
             
             # password check
             res = pwdCheck(password1)
@@ -167,24 +243,59 @@ def signUp():
                 hashed_password = bcrypt.hashpw(password1.encode(), salt)
                 current_time = int(datetime.now().timestamp())
                 print(username,hashed_password,salt,current_time)
-                new_user = Users(username, hashed_password, salt, 0, current_time)
+                new_user = Users(username, hashed_password, salt, 0, 0, 0)
                 db.session.add(new_user)
                 db.session.commit()
-                flash("Your account is successfully created!", "info")
+                flash("Your account is successfully created!", "success")
                 return redirect(url_for("login"))
             else :
                 if res['common_pwd'] :
-                    flash('Password chosen is too common, please use something more difficult to guess!')
+                    flash('Password chosen is too common, please use something more difficult to guess!', "error")
                 else:
-                    flash('Password must be between 8 to 64 characters with at least 1 Digit, 1 Upper Case Alphabet, 1 Lower Case Alphabet and 1 Special Character')
+                    flash('Password must be between 8 to 64 characters with at least 1 Digit, 1 Upper Case Alphabet, 1 Lower Case Alphabet and 1 Special Character' , "error")
                 return render_template("create_acc.html")
             
-            return render_template("create_acc.html")
+            return redirect(url_for("signUp"))
     else:
         return render_template("create_acc.html")
 
+@app.route("/dashboard", methods=["GET"])
+def dashboard():
+    print("dashboard")
+    auth_token = request.cookies.get('auth')
+    print(auth_token)
+    if auth_token is None or RevokedTokens.query.filter_by(revoked_token=auth_token).first():
+        g.user = None
+        flash("Please log in", "error")
+        return redirect(url_for("login"))
+    else:
+        res = decode_auth_token(auth_token)
+        print(res)
+        if isinstance(res, int) :
+            g.user = Users.query.filter_by(id=res).first()
+            return render_template("dashboard.html")
+        else :
+            g.user = None
+            flash(res, "error")
+            return redirect(url_for("login"))
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    print("logout")
+    auth_token = request.cookies.get('auth').strip()
+    print(auth_token)
+    if auth_token is None or RevokedTokens.query.filter_by(revoked_token=auth_token).first():
+        flash("Please log in", "error")
+        return redirect(url_for("login"))
+    else:
+        new_revoke = RevokedTokens(auth_token, int(datetime.now().timestamp()))
+        db.session.add(new_revoke)
+        db.session.commit()
+        flash("Successfully Logged Out", "success")
+        return redirect(url_for("login"))
+
 if __name__ == "__main__":
-    # db.drop_all()
+    #db.drop_all()
     db.create_all()
     context = ('cert.pem','key.pem')
     app.run(ssl_context=context, debug=True)
